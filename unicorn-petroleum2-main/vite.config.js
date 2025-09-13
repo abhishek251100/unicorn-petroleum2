3f50bfc23f79a467533c9fd8627c549dbccb05fd
AbhishekSingh2511 <abhishek.singh@thestarterlabs.com>
2025-09-13 19:26:32 +0530

    vercel chang5

--- unicorn-petroleum2-main/vite.config.js
+++ unicorn-petroleum2-main/vite.config.js
@@ -5,15 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-  base: '/',
 });
